0e128be8d891570009b8e95a8b639e25e57a6162
goreleaserbot <goreleaserbot@thisisjonathan.com>
2018-11-16 09:48:51 +0000

    Brew formula update for rif version 0.4.3

--- rif.rb
+++ rif.rb
@@ -1,15 +1,11 @@
 class Rif < Formula
   desc "File-based HTTP client for API developers"
   homepage "https://jonathanlloyd.github.io/rif"
-  url "https://github.com/jonathanlloyd/rif/releases/download/0.4.2/rif_0.4.2_darwin_amd64.tar.gz"
-  version "0.4.2"
-  sha256 "c3f26a28b505e36bd16b44e527697f8c0b61f87493b8fd8833cc6405575406e8"
+  url "https://github.com/jonathanlloyd/rif/releases/download/0.4.3/rif_0.4.3_darwin_amd64.tar.gz"
+  version "0.4.3"
+  sha256 "d2af3e3f2ee6f112596de761708418254b6bf1af5f2fcc65a8d3dedd5e26c4b1"
 
   def install
     bin.install "rif"
-  end
-
-  test do
-    
   end
 end
