7a77ee37e93c1456a6b225cffa77be660fe59cf7
goreleaserbot <goreleaserbot@thisisjonathan.com>
2019-02-17 22:33:30 +0000

    Brew formula update for rif version 0.4.4

--- rif.rb
+++ rif.rb
@@ -3,7 +3,7 @@ class Rif < Formula
   homepage "https://jonathanlloyd.github.io/rif"
   url "https://github.com/jonathanlloyd/rif/releases/download/0.4.4/rif_0.4.4_darwin_amd64.tar.gz"
   version "0.4.4"
-  sha256 "af78979250b65190dd0d909efd68ac4c2f7fc25337701f2b097772f12a48f187"
+  sha256 "2078ae9a82dafe288ddedc12c802ef7c7bd63c4a79bb1d0b038674a79ccb72a7"
 
   def install
     bin.install "rif"
